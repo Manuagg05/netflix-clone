320610c65ab6cba1141717c3cb4cd6f83a621352
manu aggarwal <manuaggarwal2001@gmail.com>
2022-10-18 17:19:40 +0530

    done

---
+++ src/Nav.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import "./nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return 
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav-black"}`}>
+      <img
+        className="nav-logo"
+        src="https://res.cloudinary.com/dltxupvq4/image/upload/v1632993826/netflix_logo_m2kje4.png"
+        alt="netflix logo"
+      />
+
+      <img
+        className="nav-avatar"
+        src="https://res.cloudinary.com/dltxupvq4/image/upload/v1632994067/netflix_avatar_nmmhgz.png"
+        alt="nav avatar"
+      />
+    </div>
+  );
+}
+
+export default Nav;
---
+++ src/request.js
@@ -0,0 +1,16 @@
+const api_key = "2d310be1aab040bb741fd0485167c8ad";
+const request = {
+  fetchTrending: `/trending/all/week?api_key=${api_key}&language=en-US`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${api_key}&with_networks=213`,
+  fetchTopRated: `/discover/movie?api_key=${api_key}&language=en-US`,
+  fetchActionMovies: `/discover/movie?api_key=${api_key}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${api_key}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${api_key}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${api_key}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${api_key}&with_genres=99`,
+};
+
+ export default request;
+
+
+ 
